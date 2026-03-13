b0701cfed5e6f591ac9033e193d5f8d4f29f1a58
antmarwas <wasikowska.antonina@gmail.com>
2026-03-13 09:10:13 +0100

    feat: first function of the game

--- project1/Octospawn.py
+++ project1/Octospawn.py
@@ -1,6 +1,7 @@
-from rdflib.plugins.sparql.parserutils import value
 
 from easyAI import TwoPlayerGame
+
+import random
 
 # Convert D7 to (3,6) and back...
 to_string = lambda move: " ".join(
@@ -29,38 +30,82 @@ class Octospawn(TwoPlayerGame):
 
         self.players = players
         self.current_player = 1
+        self.removed_pawns = {1: [], 2: []} # klucze to index gracza, wartosc to index pionka
+        self.start_positions = {
+            1: {x: p[0][x] for x in range(len(p[0]))},
+            2: {x: p[1][x] for x in range(len(p[1]))},
+        }
 
     def possible_moves(self):
         moves = []
-        opponent_pawns = self.opponent.pawns.values()  # lista pionków przeciwnika
-        d = self.player.direction #  kierunek ruchu aktualnego gracza
-        for i, j in self.player.pawns.values():
-            if (i + d, j) not in opponent_pawns:  ## idziemy jedno pole do przodu   || warto by było sprawdzić też
-                # czy to nie jest granica planszy, albo czy nasz pionek tam nie stoi, bo na razie mamy tylko != opponent_paws
-                moves.append(((i, j), (i + d, j)))
-            if (i + d, j + 1) in opponent_pawns:  # skos w prawo
-                moves.append(((i, j), (i + d, j + 1)))
-            if (i + d, j - 1) in opponent_pawns: # skos w lewo
-                moves.append(((i, j), (i + d, j - 1)))
+        d = self.player.direction
+        M, N = self.size
 
-        return list(map(to_string, [(i, j) for i, j in moves]))  # napisy zwraca do wyświetlenia dla użytkownika
+        own_pawns = set(self.player.pawns.values())
+        opponent_pawns = set(self.opponent.pawns.values())
+
+        for i, j in self.player.pawns.values():
+            # ruch do przodu
+            ni, nj = i + d, j
+            if 0 <= ni < M and 0 <= nj < N:
+                if (ni, nj) not in own_pawns and (ni, nj) not in opponent_pawns:
+                    moves.append(((i, j), (ni, nj)))
+
+            # bicie po skosie w prawo
+            ni, nj = i + d, j + 1
+            if 0 <= ni < M and 0 <= nj < N:
+                if (ni, nj) in opponent_pawns:
+                    moves.append(((i, j), (ni, nj)))
+
+            # bicie po skosie w lewo
+            ni, nj = i + d, j - 1
+            if 0 <= ni < M and 0 <= nj < N:
+                if (ni, nj) in opponent_pawns:
+                    moves.append(((i, j), (ni, nj)))
+
+        return [to_string(m) for m in moves]
 
     def make_move(self, move):
-        removed_pawns = []
-        move = list(map(to_tuple, move.split(" ")))  # zamieniamy A1 na współrzedne
-        ind = self.player.pawns.values().index(move[0])  # przesuwamy zawsze pierwszy pionek?
-        self.player.pawns[ind] = move[1]  # zmiana pozycji pionak na planszy
 
-        if move[1] in self.opponent.pawns:  # czy było bicie?
-            removed_pawns.append(self.opponent.pawns.key().index(move[0]))
-            self.opponent.pawns.remove(move[1])
+        move = list(map(to_tuple, move.split(" ")))  # zamieniamy A1 na współrzędne
 
+        source = move[0]
+        target = move[1]
+
+        ind = None
+        for idx, pos in self.player.pawns.items():
+            if pos == source:
+                ind = idx
+                break
+
+        captured_id = None
+
+        for idx, pos in list(self.opponent.pawns.items()):
+            if pos == target:
+                captured_id = idx
+                break
+
+        self.player.pawns[ind] = target  # zmiana pozycji pionka na planszy
+
+        if captured_id is not None:  # czy było bicie?
+            del self.opponent.pawns[captured_id]
+            self.removed_pawns[self.opponent_index].append(captured_id)
+
+            # losujemy 10% tylko po zbiciu
+            if self.removed_pawns[self.opponent_index] and random.random() < 1.0:
+                pawn_id = random.choice(self.removed_pawns[self.opponent_index])
+                start_pos = self.start_positions[self.opponent_index][pawn_id]
+
+                # sprawdzamy, czy pole startowe jest wolne
+                if start_pos not in self.player.pawns.values() and start_pos not in self.opponent.pawns.values():
+                    self.opponent.pawns[pawn_id] = start_pos
+                    self.removed_pawns[self.opponent_index].remove(pawn_id)
 
 
 
 
     def lose(self):
-        return any([i == self.opponent.goal_line for i, j in self.opponent.pawns]) or (  # spreawdzamy czy aktualny gracz przegrał
+        return any([i == self.opponent.goal_line for i, j in self.opponent.pawns.values()]) or (  # spreawdzamy czy aktualny gracz przegrał
             self.possible_moves() == []
         )
 
@@ -70,8 +115,8 @@ class Octospawn(TwoPlayerGame):
     def show(self): # wyświetlanie planszy w terminalu
         f = (
             lambda x: "1"
-            if x in self.players[0].pawns
-            else ("2" if x in self.players[1].pawns else ".")
+            if x in self.players[0].pawns.values()
+            else ("2" if x in self.players[1].pawns.values() else ".")
         )
         print(
             "\n".join(
@@ -81,8 +126,9 @@ class Octospawn(TwoPlayerGame):
                 ]
             )
         )
-        print(self.players[0].pawns[0])
+        print(self.players[0].pawns)
         print(self.players[1].pawns)
+        print(self.removed_pawns)
 
 
 if __name__ == "__main__":
@@ -90,6 +136,6 @@ if __name__ == "__main__":
 
     scoring = lambda game: -100 if game.lose() else 0
     ai = Negamax(10, scoring)
-    game = Octospawn([AI_Player(ai), AI_Player(ai)])
+    game = Octospawn([Human_Player(), Human_Player()])
     game.play()
     print("player %d wins after %d turns " % (game.opponent_index, game.nmove))
